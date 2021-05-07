bf46e00b51262309e62cd6cea025d2339943a0ef
siwei-yuan <siwei_yuan@hotmail.com>
2021-05-07 14:59:16 +0800

    Initialization of the project with Antd installed and imported

---
+++ library_simulator_frontend/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import Antd from 'ant-design-vue'// import Ant Design Vue
+import 'ant-design-vue/dist/antd.css' // import Ant Design Vue style sheet
+
+Vue.use(Antd)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
