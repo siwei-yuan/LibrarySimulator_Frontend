9ab2d06e81aa9108922f95ed76f6bf2b6c9179ca
siwei-yuan <siwei_yuan@hotmail.com>
2021-05-10 17:38:18 +0800

    Added Axios, http request, real-time filter & improvements

--- library_simulator_frontend/config/index.js
+++ library_simulator_frontend/config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/room': {
+        target: 'http://cuoshen.online:443/room',
+        changeOrigin: true,
+        pathRewrite: {'^/room': ''}
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- library_simulator_frontend/src/views/studyroom/index.vue
+++ library_simulator_frontend/src/views/studyroom/index.vue
@@ -4,8 +4,8 @@
       <a-icon type="filter" theme="twoTone" style="display:inline; fontSize:20px; margin-right:5px"/>
       <p style="display:inline; font-size:16px; margin-right: 10px">Filter By Room: </p>
       <a-select mode="tags" style="width: 300px" placeholder="Select Rooms (no filter by default)" @change="handlefilter">
-        <a-select-option v-for="room in roomList" :key="room.Name" :value="room.Name">
-          {{room.Name}}
+        <a-select-option v-for="name in nameList" :key="name" :value="name">
+          {{name}}
         </a-select-option>
       </a-select>
     </div>
@@ -15,63 +15,51 @@
       :data-source="roomList"
       row-key="Name"
       :title="()=>{return 'ALL ROOMS\' STATUS'}"
+      :loading="listLoading"
     >
       <template v-for="i in tableScopes" :slot="i" slot-scope="record,table">
         <a-popconfirm
-          v-if="record.slice(1) === 'true'"
+          v-if="record"
           title="Are you sure to reserve this study room?"
           ok-text="Yes"
           cancel-text="No"
           :key="i"
-          @confirm="makeReservation(record,table.Name)"
+          @confirm="makeReservation(record,table.Name,i)"
         >
-          <a-button type="primary">
+          <a-button type="primary" size="small">
             Reserve
           </a-button>
         </a-popconfirm>
         <a-tag v-else color="volcano" :key="i">
+          <a-icon type="close-circle" />
           FULL
         </a-tag>
       </template>
     </a-table>
+    <div class="room-view">
+      <a-skeleton :loading="listLoading" />
+      <a-row :gutter="16">
+        <a-col :span="3" v-for="room in roomList" :key="room.Name">
+          <div class="room-box">
+            {{room.Name}}
+          </div>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   data () {
     return {
       tableScopes: ['status0', 'status1', 'status2', 'status3', 'status4', 'status5', 'status6'],
       roomNameFiltered: '',
-      roomList: [
-        {
-          'Name': 'Hedrick',
-          'ID': 42,
-          'Availability': [
-            '1false',
-            '2false',
-            '3false',
-            '4true',
-            '5alse',
-            '6false',
-            '7false'
-          ]
-        },
-        {
-          'Name': 'Sunset',
-          'ID': 43,
-          'Availability': [
-            '1false',
-            '2false',
-            '3true',
-            '4false',
-            '5false',
-            '6true',
-            '7false'
-          ]
-        }
-      ],
+      listLoading: false,
+      roomList: [],
+      nameList: [],
       columns: [
         {
           title: 'Room Name',
@@ -115,15 +103,65 @@ export default {
       ]
     }
   },
-
+  created () {
+    this.getList(null)
+  },
   methods: {
     handlefilter (value) {
-      console.log(value)
+      this.getList(value)
     },
-    makeReservation (time, room) {
+    makeReservation (time, room, i) {
       console.log(time)
       console.log(room)
+      console.log(i)
+    },
+    getList (value) {
+      this.roomList = []
+      this.listLoading = true
+      axios.get('/room').then(res => {
+        console.log(res.data)
+        if (value === null || value.length === 0) {
+          this.roomList = res.data
+          for (let k = 0; k < res.data.length; k++) {
+            this.nameList[k] = res.data[k].Name
+          }
+        } else {
+          for (let i = 0; i < res.data.length; i++) {
+            for (let j = 0; j < value.length; j++) {
+              console.log(value[j] + ':' + res.data[i].Name)
+              if (value[j] === res.data[i].Name) {
+                this.roomList.push(res.data[i])
+              }
+            }
+          }
+        }
+        this.listLoading = false
+      })
     }
   }
 }
 </script>
+
+<style scoped>
+.room-view {
+  background: #e9f8ff;
+  padding: 10px;
+  border-radius: 6px;
+  border:1px solid #00a2ff;
+}
+.room-box {
+  font-size: larger;
+  font-weight: 600;
+  color: white;
+  background: #4fcabada;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  border-radius: 6px;
+}
+.room-box.full {
+  background: #ff6e6e9f;
+}
+</style>
