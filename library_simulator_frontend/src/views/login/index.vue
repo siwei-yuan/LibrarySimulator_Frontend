d78b229df06f25dc8658582ba738cee118f95d92
siwei-yuan <siwei_yuan@hotmail.com>
2021-05-21 22:48:41 +0800

    Improve login ui, add help message

--- library_simulator_frontend/src/views/login/index.vue
+++ library_simulator_frontend/src/views/login/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="app">
+    <div style="height: 200px">
+    </div>
+    <div class="header">
+      <h2 style="color:antiquewhite">Welcome To Library Reservation System</h2>
+    </div>
     <div class="login-wrapper">
       <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 16 }" @submit="handleSubmit">
         <a-form-item label="Username">
@@ -13,9 +18,16 @@
           />
         </a-form-item>
         <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
-          <a-button type="primary" html-type="submit" icon="login" style="margin-bottom: 8%">
+          <a-button type="primary" html-type="submit" icon="login" shape="round" style="margin-bottom: 8%; box-shadow: 1px 1px 5px 2px #0e132969;">
             LOGIN
           </a-button>
+          <a-popover>
+            <template slot="content">
+              <p>If first time logging in, you will be auto-registered.</p>
+              <p>Otherwise, correct username and password must be provided.</p>
+            </template>
+            <a-button type="link" icon="question-circle" style="margin-left: 140px; margin-top: 15px; position: absolute" />
+          </a-popover>
         </a-form-item>
       </a-form>
     </div>
@@ -53,13 +65,17 @@ export default {
   border-radius: 30px;
   padding-left: 2%;
   padding-top: 2%;
-  box-shadow: 5px 5px 10px 10px #e6646450;
+  box-shadow: 2px 2px 120px 5px #0e132969;
 }
 
-.app {
-  background-image: linear-gradient(#e66465, #9198e5);
-  padding-top: 200px;
-  padding-bottom: 350px;
-  width: 100%;
+.header {
+  text-align: center;
+  line-height: 60px;
+  height: 120px;
+  margin-left: 35%;
+  margin-right: 35%;
+  margin-bottom: -60px;
+  background: rgb(71, 120, 167);
+  border-radius: 30px;
 }
 </style>
