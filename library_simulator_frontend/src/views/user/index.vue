1f5b772999eebb81aeec2251bce316fc030d7bbc
WeizhenWang-1210 <wwz011210@g.ucla.edu>
2021-05-27 19:33:05 +0800

    Weizhen Wang's 1st commit
    Thorough reading/interpretation of Jerry Yuan's code; Added template for the sample user profile; Implemented a visibility-protected password section.
    
    Todo:
    Implement a function to change the password, more style design.

--- library_simulator_frontend/src/views/user/index.vue
+++ library_simulator_frontend/src/views/user/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <h5> This Is User Page </h5>
+  <div>
+    <p><a-avatar
+      :size="128"
+      :style="{backgroundColor: '#2e87c5', verticalAlign: 'middle'}"
+      style="margin-left:46%; margin-top:-0px;font-size:40px"
+    >
+      Jerry
+    </a-avatar></p>
+   <ul style="list-style-type:circle;margin-left:40%">
+    <li>User Name: Jerry Yuan </li>
+    <li>User ID: 114514191</li>
+    <li>
+      <a-button type="primary" @mouseover = "showp()" @mouseout="clearp()">Hover to View Password</a-button>
+      <p id="password"></p>
+    </li>
+  </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+  },
+  methods: {
+    showp () { document.getElementById('password').innerHTML = '1919810' },
+    clearp () { document.getElementById('password').innerHTML = '' }
+  }
+}
+</script>
